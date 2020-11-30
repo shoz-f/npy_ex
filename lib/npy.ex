4d73c9042234e8d7f3af140d601908a1932c0292
shozo fukuda <fukuda_shozo@yahoo.co.jp>
2020-12-01 00:39:09 +0900

    starting the project

---
+++ lib/npy.ex
@@ -0,0 +1,106 @@
+defmodule Npy do
+  @moduledoc """
+  Peeking *.npy files.
+  """
+
+  # npy data
+  defstruct descr: nil, fortran_order: false, shape: [], data: <<>>
+
+  alias Npy
+
+  @doc """
+  load npy
+  """
+  def load(fname) do
+    res = File.open(fname, [:read], fn file ->
+      with <<0x93, "NUMPY", mejour, minor>> <- IO.binread(file, 8)
+      do
+        header_len = case mejour do
+          1 -> <<len::little-16>> = IO.binread(file, 2); len
+          _ -> <<len::little-32>> = IO.binread(file, 4); len
+        end
+
+        header = IO.binread(file, header_len)
+        descr = case Regex.run(~r/'descr': '([<=|>]?\w+)',/, header) do
+          [_, descr] -> descr
+          _ -> nil
+        end
+        fortran_order = case Regex.run(~r/'fortran_order': (True|False),/, header) do
+          [_, "True" ] -> true
+          [_, "False"] -> false
+          _ -> nil
+        end
+        shape = case Regex.run(~r/'shape': \((\d+(,\s*\d+)*)\),/, header) do
+          [_, shape|_] -> String.split(shape, ~r/,\s*/) |> Enum.map(&String.to_integer/1)
+          _ -> nil
+        end
+
+        {:ok, %Npy{descr: descr, fortran_order: fortran_order, shape: shape, data: IO.binread(file, :all)}}
+      else
+        _ -> {:error, "illegal npy file"}
+      end
+    end)
+
+    case res do
+      {:ok, fun_res} -> fun_res
+      err -> err
+    end
+  end
+
+  @doc """
+  save npy
+  """
+  def save(fname, %Npy{}=npy) do
+    header =
+      "{'descr': '#{npy.descr}', 'fortran_order': #{if npy.fortran_order,do: "True",else: "False"}, 'shape': (#{Enum.join(npy.shape, ", ")}), }"
+      |> (&(&1 <> String.duplicate(" ", 63-rem(byte_size(&1)+10, 64)) <> "\n")).()
+      |> (&(<<0x93,"NUMPY",1,0,byte_size(&1)::little-integer-16>> <> &1)).()
+
+    with {:ok, file} <- File.open(fname, [:write])
+    do
+      IO.binwrite(file, header)
+      IO.binwrite(file, npy.data)
+      File.close(file)
+    end
+  end
+
+  @doc """
+  convert to nested list
+  """
+  def to_list(%Npy{descr: descr, shape: shape, data: data}) do
+    flat_list = case descr do
+      "<f4" -> for <<x::little-float-32 <- data>> do x end
+      "<i4" -> for <<x::little-integer-32 <- data>> do x end
+      _ -> nil
+    end
+
+    if (flat_list), do: list_forming(Enum.reverse(shape), flat_list)
+  end
+  def to_list(_), do: nil
+
+  defp list_forming([_],          formed), do: formed
+  defp list_forming([size|shape], formed), do: list_forming(shape, Enum.chunk_every(formed, size))
+
+  @doc """
+  convert from nested list
+  """
+  def from_list(x, descr) when length(x) > 0 do
+    to_binary = case descr do
+      "<f4" -> fn x,acc -> acc <> <<x::little-float-32>> end
+      "<i4" -> fn x,acc -> acc <> <<x::little-integer-32>> end
+      _ -> nil
+    end
+
+    if to_binary do
+      %Npy{
+        descr: descr,
+        shape: calc_shape(x),
+        data:  Enum.reduce(List.flatten(x), <<>>, to_binary)
+      }
+    end
+  end
+  def from_list(_, _), do: nil
+
+  def calc_shape([item|_]=x), do: [Enum.count(x)|calc_shape(item)]
+  def calc_shape(_),          do: []
+end
---
+++ test/npy_test.exs
@@ -0,0 +1,50 @@
+defmodule NpyTest do
+  use ExUnit.Case
+  doctest Npy
+
+  test "load dog_output0.npy" do
+    assert {:ok, npy} = Npy.load("test/dog_output0.npy")
+    assert %Npy{descr: "<f4", fortran_order: false, shape: [1,10647,4]} = npy
+  end
+
+  test "load pred0.npy" do
+    assert {:ok, npy} = Npy.load("test/pred0.npy")
+    assert %Npy{descr: "<f4", fortran_order: false, shape: [1,10647,4]} = npy
+  end
+
+  test "load illegal file" do
+    assert {:error, "illegal npy file"} = Npy.load("test/test_helper.exs")
+  end
+
+  test "load absent file" do
+    assert {:error, :enoent} = Npy.load("test/pred.npy")
+  end
+
+  test "to_list" do
+    assert {:ok, npy} = Npy.load("test/dog_output0.npy")
+    assert [[[4.177847385406494, 2.4681198596954346,7.7223896980285645, 4.816765785217285]|_]] = Npy.to_list(npy)
+  end
+  
+  test "from_list: vector" do
+    assert %Npy{descr: "<i4", shape: [5], data: data} = Npy.from_list([1,2,3,4,5], "<i4")
+    assert data == <<1,0,0,0, 2,0,0,0, 3,0,0,0, 4,0,0,0, 5,0,0,0>>
+  end
+  
+  test "from_list: matrix" do
+    assert %Npy{descr: "<f4", shape: [2,2], data: data} = Npy.from_list([[1.0, 0.0],[0.0, 1.0]], "<f4")
+    assert <<1.0::little-32, 0.0::little-32, 0.0::little-32, 1.0::little-32>> == data
+  end
+
+  test "from_list: tensor" do
+    assert {:ok, npy} = Npy.load("test/dog_output0.npy")
+    assert npy == Npy.from_list(Npy.to_list(npy), "<f4")
+  end
+
+  test "from_list: []" do
+    assert nil == Npy.from_list([], "<f4")
+  end
+  
+  test "from_list: <p16" do
+    assert nil == Npy.from_list([1,2,3,4,5], "<i16")
+  end
+end
